e09852f8197df4dc17e9fc96dd879af49c4a7574
where.liu <where.liu@huolala.cn>
2021-04-30 13:42:58 +0800

    feat: save

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { Button } from "@material-ui/core";
 export default function Index() {
   return (
     <div className="mt-10">
-      <span>haha</span>
+      <Button variant="contained" color="primary">
+        按钮
+      </Button>
     </div>
   );
 }
