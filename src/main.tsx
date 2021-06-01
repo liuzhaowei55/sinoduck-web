1ae70adba01e7acc80361fcba281979c3e74cb9a
where.liu <where.liu@huolala.cn>
2021-06-01 14:45:45 +0800

    feat: save

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./main.css";
 import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
+    <CssBaseline />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
