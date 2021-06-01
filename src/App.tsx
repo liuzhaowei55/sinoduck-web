1dc98a43e355f2965882f61b951004e8055f0499
where.liu <where.liu@huolala.cn>
2021-06-01 15:34:07 +0800

    feat: save

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import Box from "@material-ui/core/Box";
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Index} />
-    </Switch>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route exact path="/" component={Index} />
+      </Switch>
+    </Box>
   );
 }
